cba5aa440d671624e7f01f6ec3a365c60405ffa1
AlexanderTL0343 <145624722+AlexanderTL0343@users.noreply.github.com>
2024-06-10 02:14:27 -0600

    Add files via upload

---
+++ Practica2_Desarrollo_Web/src/main/java/com/Practica2_Desarrollo_Web/demo/Practica2DesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package com.Practica2_Desarrollo_Web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2DesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2DesarrolloWebApplication.class, args);
+	}
+
+}
